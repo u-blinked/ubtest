c81f0cbdd6742ebdc9cb219afe2fd72a05691479
110w110 <110w110.official@gmail.com>
2022-10-24 21:54:51 +0900

    Initial Commit

---
+++ ubtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ubtest
+//
+//  Created by 한태희 on 2022/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
